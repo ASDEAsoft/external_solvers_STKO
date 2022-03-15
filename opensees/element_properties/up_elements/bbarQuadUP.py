d066e318c77e30fe82b8538c634286ac5f153e6f
asdea-ngermano <97520253+asdea-ngermano@users.noreply.github.com>
2022-03-15 10:10:56 +0100

    Update bbarQuadUP.py
    Updated Exception message

--- opensees/element_properties/up_elements/bbarQuadUP.py
+++ opensees/element_properties/up_elements/bbarQuadUP.py
@@ -166,7 +166,7 @@ def writeTcl(pinfo):
 	if phys_prop.XObject.Xnamespace != 'materials.nD':
 		raise Exception('Physical property must be "materials.nD" and not: "{}"'.format(phys_prop.XObject.Xnamespace))
 	if (elem.geometryFamilyType() != MpcElementGeometryFamilyType.Quadrilateral) or (len(elem.nodes) != 4):
-		raise Exception('Invalid element type for "{}", expected Quadrilateral with 4 nodes, given {} with {} nodes'.format(elem.geometryFamilyType(), len(elem.nodes)))
+		raise Exception('Invalid element type for "{}", expected Quadrilateral with 4 nodes, given {} with {} nodes'.format(elem_prop.name, elem.geometryFamilyType(), len(elem.nodes)))
 	
 	tag = elem.id
 	matTag = phys_prop.id
