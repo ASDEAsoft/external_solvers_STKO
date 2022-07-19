20c3c9b8762b13b332e11a870621abb1f4f12531
Massimo Petracca <mazuma88@hotmail.com>
2022-07-19 17:42:09 +0200

    implementing override in tester enablers

--- opensees/physical_properties/utils/tester/EnableTester3D.py
+++ opensees/physical_properties/utils/tester/EnableTester3D.py
@@ -5,6 +5,7 @@
 # from opensees.physical_properties.utils.tester.EnableTester3D import *
 
 from opensees.physical_properties.utils.tester.TesterND import NDTraits, TesterNDWidget
+from opensees.utils.override_utils import get_function_from_module
 
 class Tester3DGuiGlobals:
 	# stores a reference to the gui generated for this object
@@ -16,13 +17,19 @@ def __removeGui():
 		Tester3DGuiGlobals.gui.deleteLater()
 		Tester3DGuiGlobals.gui = None
 
+_onEditorClosing = get_function_from_module(__name__, 'onEditorClosing')
 def onEditorClosing(editor, xobj):
+	if _onEditorClosing: _onEditorClosing(editor, xobj)
 	__removeGui()
 
+_onEditFinished = get_function_from_module(__name__, 'onEditFinished')
 def onEditFinished(editor, xobj):
+	if _onEditFinished: _onEditFinished(editor, xobj)
 	if Tester3DGuiGlobals.gui is not None:
 		Tester3DGuiGlobals.gui.onEditFinished()
 
+_onEditBegin = get_function_from_module(__name__, 'onEditBegin')
 def onEditBegin(editor, xobj):
+	if _onEditBegin: _onEditBegin(editor, xobj)
 	__removeGui()
 	Tester3DGuiGlobals.gui = TesterNDWidget(NDTraits.D3, editor, xobj)
