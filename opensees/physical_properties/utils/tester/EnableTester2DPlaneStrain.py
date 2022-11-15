daecb3f5af25c7040840b79067d0dc08d01049ce
Massimo Petracca <mazuma88@hotmail.com>
2022-11-15 10:54:04 +0100

    fixing tester methods override

--- opensees/physical_properties/utils/tester/EnableTester2DPlaneStrain.py
+++ opensees/physical_properties/utils/tester/EnableTester2DPlaneStrain.py
@@ -5,7 +5,6 @@
 # from opensees.physical_properties.utils.tester.EnableTester2DPlaneStrain import *
 
 from opensees.physical_properties.utils.tester.TesterND import NDTraits, TesterNDWidget
-from opensees.utils.override_utils import get_function_from_module
 
 class Tester2DPlaneStrainGuiGlobals:
 	# stores a reference to the gui generated for this object
@@ -17,19 +16,13 @@ def __removeGui():
 		Tester2DPlaneStrainGuiGlobals.gui.deleteLater()
 		Tester2DPlaneStrainGuiGlobals.gui = None
 
-_onEditorClosing = get_function_from_module(__name__, 'onEditorClosing')
 def onEditorClosing(editor, xobj):
-	if _onEditorClosing: _onEditorClosing(editor, xobj)
 	__removeGui()
 
-_onEditFinished = get_function_from_module(__name__, 'onEditFinished')
 def onEditFinished(editor, xobj):
-	if _onEditFinished: _onEditFinished(editor, xobj)
 	if Tester2DPlaneStrainGuiGlobals.gui is not None:
 		Tester2DPlaneStrainGuiGlobals.gui.onEditFinished()
 
-_onEditBegin = get_function_from_module(__name__, 'onEditBegin')
 def onEditBegin(editor, xobj):
-	if _onEditBegin: _onEditBegin(editor, xobj)
 	__removeGui()
 	Tester2DPlaneStrainGuiGlobals.gui = TesterNDWidget(NDTraits.D2_PSTRAIN, editor, xobj)
--- opensees/physical_properties/utils/tester/EnableTester2DPlaneStress.py
+++ opensees/physical_properties/utils/tester/EnableTester2DPlaneStress.py
@@ -5,7 +5,6 @@
 # from opensees.physical_properties.utils.tester.EnableTester2DPlaneStress import *
 
 from opensees.physical_properties.utils.tester.TesterND import NDTraits, TesterNDWidget
-from opensees.utils.override_utils import get_function_from_module
 
 class Tester2DPlaneStressGuiGlobals:
 	# stores a reference to the gui generated for this object
@@ -17,19 +16,13 @@ def __removeGui():
 		Tester2DPlaneStressGuiGlobals.gui.deleteLater()
 		Tester2DPlaneStressGuiGlobals.gui = None
 
-_onEditorClosing = get_function_from_module(__name__, 'onEditorClosing')
 def onEditorClosing(editor, xobj):
-	if _onEditorClosing: _onEditorClosing(editor, xobj)
 	__removeGui()
 
-_onEditFinished = get_function_from_module(__name__, 'onEditFinished')
 def onEditFinished(editor, xobj):
-	if _onEditFinished: _onEditFinished(editor, xobj)
 	if Tester2DPlaneStressGuiGlobals.gui is not None:
 		Tester2DPlaneStressGuiGlobals.gui.onEditFinished()
 
-_onEditBegin = get_function_from_module(__name__, 'onEditBegin')
 def onEditBegin(editor, xobj):
-	if _onEditBegin: _onEditBegin(editor, xobj)
 	__removeGui()
 	Tester2DPlaneStressGuiGlobals.gui = TesterNDWidget(NDTraits.D2_PSTRESS, editor, xobj)
