7de7c8c4b76d8e4f8f6adca739ea65599036825f
Massimo Petracca <mazuma88@hotmail.com>
2025-12-30 14:22:13 +0100

    clean opspro

--- opspro/physical_properties/MaterialDetails/MaterialClass.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-from opspro.utils.stko_serialization import serializable
-
-@dataclass
-@serializable
-class MaterialClass:
-    E: float = 0.0
-    v: float = 0.0
-    rho: float = 0.0
--- opspro/physical_properties/__init__.py
+++
@@ -1,1 +0,0 @@
-# dont touch my comment
